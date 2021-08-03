f6abee74654e82d306543b140fdc9b0c72a5e91b
fedkey <fedkey@sina.com>
2021-08-04 01:26:31 +0800

    submit hi

---
+++ hi/target/debug/deps/hi.d
@@ -0,0 +1,5 @@
+E:\learn\learn_rust\hi\target\debug\deps\hi.exe: src\main.rs
+
+E:\learn\learn_rust\hi\target\debug\deps\hi.d: src\main.rs
+
+src\main.rs:
---
+++ hi/target/debug/hi.d
@@ -0,0 +1,1 @@
+E:\learn\learn_rust\hi\target\debug\hi.exe: E:\learn\learn_rust\hi\src\main.rs
